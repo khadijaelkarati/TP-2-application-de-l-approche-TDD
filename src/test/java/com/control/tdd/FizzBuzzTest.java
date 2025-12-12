c22b32cd153c9f1e747fc2d13409c866460916de
khadijaelkarati <khadijaelkarati474@gmail.com>
2025-12-12 20:12:27 +0100

    cycle 7 : Test paramétré

--- src/test/java/com/control/tdd/FizzBuzzTest.java
+++ src/test/java/com/control/tdd/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 package com.control.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,5 +37,12 @@ class FizzBuzzTest {
     void FizzBuzz_de_15(){
         String result = FizzBuzz.FizzBuzz_de(15);
         assertThat(result).isEqualTo("FizzBuzz");
+    }
+    @ParameterizedTest
+    @CsvSource({"1,1",
+            "2,2","3,Fizz","5,Buzz","18,Fizz","25,Buzz","30,FizzBuzz"})
+    void FizzBuzzparam(int in,String r) {
+        String result = FizzBuzz.FizzBuzz_de(in);
+        assertThat(result).isEqualTo(r);
     }
 }
